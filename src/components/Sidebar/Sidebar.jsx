7bb475435e1f1c0e2bd1aecfc116be1f78826b10
Brandon Ngo <okibrandon@hotmail.com>
2021-02-16 23:54:42 -0500

    Sidebar component w/o styling

---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import profileImg from '';
+import ReactDOM from 'react-dom';
+
+class Sidebar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      about: true,
+      projects: false,
+      education: false,
+      experience: false
+    };
+  }
+
+  render() {
+    return (
+      <section>
+        <img src={profileImg} alt="profile picture"/>
+
+        <div>
+          <a href="">About</a>
+          <a href="">Projects</a>
+          <a href="">Education</a>
+          <a href="">Experience</a>
+        </div>
+      </section>
+
+      
+    );
+  }
+
+}
+
+export default Sidebar;
