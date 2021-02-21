402c828cf159e50cea0b8e1e10604acc9c0e7610
Brandon Ngo <okibrandon@hotmail.com>
2021-02-20 22:00:04 -0500

    added and removed react-stickynode

--- src/screens/ScreensRoot.jsx
+++ src/screens/ScreensRoot.jsx
@@ -4,16 +4,20 @@ import rootstyle from './root.css';
 import Sidebar from '../components/Sidebar/Sidebar';
 import About from '../components/About/AboutMe';
 import Projects from '../components/Projects/Projects';
+import Sticky from 'react-stickynode';
 
 class ScreensRoot extends Component {
   render() {
     return (
       <section className="screen">
-        <Sidebar className="sidebar"/>
+        <Sticky>
+          <Sidebar className="sidebar"/>
+        </Sticky>
 
         <div className="pages">
           <About className="about"/>
           <Projects className="projects"/>
+          
         </div>
 
       </section>
@@ -23,3 +27,6 @@ class ScreensRoot extends Component {
 }
 
 export default ScreensRoot;
+
+//
+//
