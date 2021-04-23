1cb74f48dc7b96a14dede581a7c3a098f1470529
Brandon Ngo <okibrandon@hotmail.com>
2021-04-23 14:23:28 -0400

    added project list

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -19,6 +19,43 @@ class Projects extends Component {
             <li>Implementation of voice and instant messaging using sockets</li>
           </ul>
         </div>
+
+        <div>
+          <h2>Food Tracker</h2>
+          <p className="info">Submitted to yuHacks 2021</p>
+          <ul>
+            <li>Keeps track of food expiration dates</li>
+            <li>Implementation of a CRUD React Native app</li>
+            <li>Uses Barcode API to automate entries</li>
+          </ul>
+        </div>
+
+        <div>
+          <h2>Social Links</h2>
+          <ul>
+            <li>Allows user to input social media accounts in one spot</li>
+            <li>Implementation of a CRUD React app</li>
+            <li></li>
+          </ul>
+        </div>
+
+        <div>
+          <h2>Cyrptocurrency Ticker</h2>
+          <ul>
+            <li>Allows user to input social media accounts in one spot</li>
+            <li>Implementation of a CRUD React app</li>
+            <li></li>
+          </ul>
+        </div>
+
+        <div>
+          <h2>URL Shortener</h2>
+          <ul>
+            <li>Allows user to input social media accounts in one spot</li>
+            <li>Implementation of a CRUD React app</li>
+            <li></li>
+          </ul>
+        </div>
     
       </section>
       
